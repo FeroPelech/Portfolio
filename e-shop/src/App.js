82c6895fd71d1608ec68bacd1fe94fa546c040c6
Frantisek Pelech <feromiro23@gmail.com>
2024-09-24 22:15:18 +0200

    Deleted all header code

--- e-shop/src/App.js
+++ e-shop/src/App.js
@@ -1,32 +1,10 @@
 import React from "react";
-import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
-import StoreIcon from "@mui/icons-material/Store";
+import Header from "./Header";
 
 const App = () => {
   return (
     <div className="App">
-      <div className="header__logo">
-        <StoreIcon className="header__logoImage" fontSize="large" />
-        <h2 className="header__logoTitle">eShop</h2>
-      </div>
-      <div className="header__search">
-        <input type="text" className="header__searchInput" />
-      </div>
-
-      <div className="header__nav">
-        <div className="nav__item">
-          <span className="nav__itemLineOne">Hello Guest</span>
-          <span className="nav__itemLineTwo">Sign In</span>
-        </div>
-        <div className="nav__item">
-          <span className="nav__itemLineOne">Your</span>
-          <span className="nav__itemLineTwo">Shop</span>
-        </div>
-        <div className="nav__item">
-          <ShoppingCartIcon fontSize="large" />
-          <span className="nav__itemLineTwo">0</span>
-        </div>
-      </div>
+      <Header />
     </div>
   );
 };
