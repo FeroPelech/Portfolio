70560d648f1d28c823a4f499285b4ddc75a59969
Frantisek Pelech <feromiro23@gmail.com>
2024-10-02 00:40:41 +0200

    Solved problem with pictures position

--- e-shop/src/Product.js
+++ e-shop/src/Product.js
@@ -3,25 +3,23 @@ import "./Product.css";
 
 export default function Product({ id, title, image, price, rating }) {
   return (
-    <div>
-      <div className="product">
-        <div className="product__info">
-          <p>{title}</p>
-          <p className="product__price">
-            <strong>{price}</strong>
-            <small>€</small>
-          </p>
-          <div className="product__rating">
-            {Array(rating)
-              .fill()
-              .map((_, i) => (
-                <p>⭐</p>
-              ))}
-          </div>
-          <img src={image} alt="Bag" />
-          <button>Add to Basket</button>
+    <div className="product">
+      <div className="product__info">
+        <p>{title}</p>
+        <p className="product__price">
+          <strong>{price}</strong>
+          <small>€</small>
+        </p>
+        <div className="product__rating">
+          {Array(rating)
+            .fill()
+            .map((_, i) => (
+              <p>⭐</p>
+            ))}
         </div>
       </div>
+      <img src={image} alt="Bag" />
+      <button>Add to Basket</button>
     </div>
   );
 }
