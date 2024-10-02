55f85acc301d981ac2442e768706433ea20d8809
Frantisek Pelech <feromiro23@gmail.com>
2024-10-03 00:32:10 +0200

    Added BrowserRouter from react-router-dom

--- e-shop/src/App.js
+++ e-shop/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "./Header";
 import Home from "./Home";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 const App = () => {
   return (
