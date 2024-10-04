4a45005cd2bbe0f798a1df1b7ae8b35166320da3
Frantisek Pelech <feromiro23@gmail.com>
2024-10-04 12:27:27 +0200

    Added Routes and solved problem with react router version from 6 to 5

--- e-shop/src/Header.js
+++ e-shop/src/Header.js
@@ -3,14 +3,17 @@ import "./Header.css";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import StoreIcon from "@mui/icons-material/Store";
 import SearchIcon from "@mui/icons-material/Search";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <div className="header">
-      <div className="header__logo">
-        <StoreIcon className="header__logoImage" fontSize="large" />
-        <h2 className="header__logoTitle">eShop</h2>
-      </div>
+      <Link to="/" style={{ textDecoration: "none" }}>
+        <div className="header__logo">
+          <StoreIcon className="header__logoImage" fontSize="large" />
+          <h2 className="header__logoTitle">eShop</h2>
+        </div>
+      </Link>
       <div className="header__search">
         <input type="text" className="header__searchInput" />
         <SearchIcon className="header__searchIcon" />
@@ -25,10 +28,12 @@ function Header() {
           <span className="nav__itemLineOne">Your</span>
           <span className="nav__itemLineTwo">Shop</span>
         </div>
-        <div className="nav__item">
-          <ShoppingCartIcon fontSize="large" />
-          <span className="nav__itemLineTwo nav__basketCount">0</span>
-        </div>
+        <Link to="/checkout" style={{ textDecoration: "none" }}>
+          <div className="nav__itemBasket">
+            <ShoppingCartIcon fontSize="large" />
+            <span className="nav__itemLineTwo nav__basketCount">0</span>
+          </div>
+        </Link>
       </div>
     </div>
   );
