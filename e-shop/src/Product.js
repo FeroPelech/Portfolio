a839063307b0cc7ed74110ca2d85174956241092
Frantisek Pelech <feromiro23@gmail.com>
2024-10-22 22:58:57 +0200

    Added const addToBasket witch contains arrow function with informations id,price,rating and image

--- e-shop/src/Product.js
+++ e-shop/src/Product.js
@@ -5,6 +5,18 @@ import { useState, useStateValue } from "./StateProvider";
 export default function Product({ id, title, image, price, rating }) {
   const [state, dispatch] = useStateValue();
 
+  const addToBasket = () => {
+    dispatch({
+      type: "ADD_TO_BASKET",
+      item: {
+        id: id,
+        image: image,
+        price: price,
+        rating: rating,
+      },
+    });
+  };
+
   return (
     <div className="product">
       <div className="product__info">
