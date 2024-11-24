506700966bb14d14eeb7c3d6ad6e600b24c5942c
Frantisek Pelech <feromiro23@gmail.com>
2024-11-24 22:40:53 +0100

    New react projet and added Star Wars info to movie constant

---
+++ movie_streaming/src/App.js
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+const App = () => {
+  const [movies, setMovies] = useState([
+    {
+      Title: "Star Wars: Episode IV - A New Hope",
+      Year: "1977",
+      imdbID: "tt0076759",
+      Type: "movie",
+      Poster:
+        "https://m.media-amazon.com/images/M/MV5BOGUwMDk0Y2MtNjBlNi00NmRiLTk2MWYtMGMyMDlhYmI4ZDBjXkEyXkFqcGc@._V1_SX300.jpg",
+    },
+    {
+      Title: "Star Wars: Episode V - The Empire Strikes Back",
+      Year: "1980",
+      imdbID: "tt0080684",
+      Type: "movie",
+      Poster:
+        "https://m.media-amazon.com/images/M/MV5BMTkxNGFlNDktZmJkNC00MDdhLTg0MTEtZjZiYWI3MGE5NWIwXkEyXkFqcGc@._V1_SX300.jpg",
+    },
+    {
+      Title: "Star Wars: Episode VI - Return of the Jedi",
+      Year: "1983",
+      imdbID: "tt0086190",
+      Type: "movie",
+      Poster:
+        "https://m.media-amazon.com/images/M/MV5BNWEwOTI0MmUtMGNmNy00ODViLTlkZDQtZTg1YmQ3MDgyNTUzXkEyXkFqcGc@._V1_SX300.jpg",
+    },
+    {
+      Title: "Star Wars: Episode VII - The Force Awakens",
+      Year: "2015",
+      imdbID: "tt2488496",
+      Type: "movie",
+      Poster:
+        "https://m.media-amazon.com/images/M/MV5BOTAzODEzNDAzMl5BMl5BanBnXkFtZTgwMDU1MTgzNzE@._V1_SX300.jpg",
+    },
+    {
+      Title: "Star Wars: Episode I - The Phantom Menace",
+      Year: "1999",
+      imdbID: "tt0120915",
+      Type: "movie",
+      Poster:
+        "https://m.media-amazon.com/images/M/MV5BODVhNGIxOGItYWNlMi00YTA0LWI3NTctZmQxZGUwZDEyZWI4XkEyXkFqcGc@._V1_SX300.jpg",
+    },
+  ]);
+  return <div>App</div>;
+};
+
+export default App;
