0cb11fad844c1daab1cbaa122ae05461b46eeba6
Frantisek Pelech <feromiro23@gmail.com>
2024-11-25 22:27:55 +0100

    Small correction

--- movie_streaming/src/App.js
+++ movie_streaming/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import MovieList from "./components/MovieList";
 
 const App = () => {
   const [movies, setMovies] = useState([
