ad87ce87dd6409d44e29dc48928fa1d52dd3a8af
Frantisek Pelech <feromiro23@gmail.com>
2024-12-29 13:46:24 +0100

    Added props to the MovieListHeading

--- movie_streaming/src/components/MovieListHeading.jsx
+++ movie_streaming/src/components/MovieListHeading.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
-const MovieListHeading = () => {
-  return <div>MovieListHeading</div>;
+const MovieListHeading = (props) => {
+  return (
+    <div className="col">
+      <h1>{props.heading}</h1>
+    </div>
+  );
 };
 
 export default MovieListHeading;
