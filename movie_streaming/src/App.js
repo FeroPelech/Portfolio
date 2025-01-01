d2f9a3eae70cb14ccda4212c19ed113cd0d501aa
Frantisek Pelech <feromiro23@gmail.com>
2025-01-01 23:30:45 +0100

    Inserting MovieListHeading call

--- movie_streaming/src/App.js
+++ movie_streaming/src/App.js
@@ -2,13 +2,16 @@ import React, { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import MovieList from "./components/MovieList";
+import MovieListHeading from "./components/MovieListHeading";
 
 const App = () => {
   const [movies, setMovies] = useState([]);
   const [searchValue, setSearchValue] = useState("");
   return (
     <div className="container-fluid movie-app">
-      <div className="row d-flex align-items-center mt-4 mb-4"></div>
+      <div className="row d-flex align-items-center mt-4 mb-4">
+        <MovieListHeading />
+      </div>
       <div className="row">
         <MovieList movies={movies} />
       </div>
