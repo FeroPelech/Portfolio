50c2e23aaab48f5694ed84df05acac3b0d8875bf
Frantisek Pelech <feromiro23@gmail.com>
2025-01-05 14:25:15 +0100

    Imported AddFavourites.jsx

--- movie_streaming/src/App.js
+++ movie_streaming/src/App.js
@@ -4,6 +4,7 @@ import "./App.css";
 import MovieList from "./components/MovieList";
 import MovieListHeading from "./components/MovieListHeading";
 import SearchBox from "./components/SearchBox";
+import AddFavourites from "./components/AddFavourites";
 
 const App = () => {
   const [movies, setMovies] = useState([]);
