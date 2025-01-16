dc604a1e5cde4ab37fd48f34ffd61adad7535dd8
Frantisek Pelech <feromiro23@gmail.com>
2025-01-16 23:18:38 +0100

    Added div

--- movie_streaming/src/components/MovieList.jsx
+++ movie_streaming/src/components/MovieList.jsx
@@ -1,9 +1,13 @@
 const MovieList = (props) => {
   return (
     <>
-      {props.movies.map((movies, index) => (
+      {props.movies.map((movie, index) => (
         <div className="image-container d-flex justify-content-start m3">
-          <img src={movies.Poster} alt="movie" />
+          <img src={movie.Poster} alt="movie" />
+          <div
+            onClick={() => props.handleFavouritesClick(movie)}
+            className="overlay d-flex align-items-center justify-content-center"
+          ></div>
         </div>
       ))}
     </>
