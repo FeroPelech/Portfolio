7ca05b89e4b798772769e840151bac2287087556
Frantisek Pelech <feromiro23@gmail.com>
2025-01-18 00:04:52 +0100

    Constant removeFavouriteMovie

--- movie_streaming/src/App.js
+++ movie_streaming/src/App.js
@@ -5,6 +5,7 @@ import MovieList from "./components/MovieList";
 import MovieListHeading from "./components/MovieListHeading";
 import SearchBox from "./components/SearchBox";
 import AddFavourites from "./components/AddFavourites";
+import RemoveFavourites from "./components/RemoveFavourites";
 
 const App = () => {
   const [movies, setMovies] = useState([]);
@@ -29,6 +30,12 @@ const App = () => {
     setFavourites(newFavouriteList);
   };
 
+  const removeFavouriteMovie = (movie) => {
+    const newFavouriteList = favourites.filter(
+      (favourite) => favourite.imdbID !== movie.imdbID
+    );
+  };
+
   return (
     <div className="container-fluid movie-app">
       <div className="row d-flex align-items-center mt-4 mb-4">
@@ -46,7 +53,11 @@ const App = () => {
         <MovieListHeading heading="Favourites" />
       </div>
       <div className="row">
-        <MovieList movies={favourites} />
+        <MovieList
+          movies={favourites}
+          handleFavouritesClick={removeFavouriteMovie}
+          favouriteComponent={RemoveFavourites}
+        />
       </div>
     </div>
   );
