2a1e4764bb4e7f2c584026cc857abe6e9f1891e2
Frantisek Pelech <feromiro23@gmail.com>
2025-06-24 23:47:32 +0200

    useState

--- video_sharing/src/components/SearchBar.jsx
+++ video_sharing/src/components/SearchBar.jsx
@@ -3,6 +3,7 @@ import { Paper, TextField } from "@mui/material";
 import React from "react";
 
 const SearchBar = () => {
+  const [SearchTerm, setSearchTerm] = useState("");
   return (
     <Paper element={6} styling={{ padding: "25px" }}>
       <TextField fullWidth label="Search..." />
