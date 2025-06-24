c703f9804c259459a314bd31ee90326c089b44bf
Frantisek Pelech <feromiro23@gmail.com>
2025-06-24 23:52:57 +0200

    Constant for handleChange

--- video_sharing/src/components/SearchBar.jsx
+++ video_sharing/src/components/SearchBar.jsx
@@ -3,10 +3,17 @@ import { Paper, TextField } from "@mui/material";
 import React from "react";
 
 const SearchBar = () => {
-  const [SearchTerm, setSearchTerm] = useState("");
+  const [searchTerm, setSearchTerm] = useState("");
+  const handleChange = (event) => setSearchTerm(event.target.value);
+
   return (
     <Paper element={6} styling={{ padding: "25px" }}>
-      <TextField fullWidth label="Search..." />
+      <TextField
+        fullWidth
+        label="Search..."
+        value={searchTerm}
+        onChange={handleChange}
+      />
     </Paper>
   );
 };
