4a866f6306e1c199b353fc283274375f249e8d59
Frantisek Pelech <feromiro23@gmail.com>
2025-06-24 23:44:48 +0200

    Paper element

--- video_sharing/src/components/SearchBar.jsx
+++ video_sharing/src/components/SearchBar.jsx
@@ -3,7 +3,7 @@ import { Paper, TextField } from "@mui/material";
 import React from "react";
 
 const SearchBar = () => {
-  return <div>SearchBar</div>;
+  return <Paper element={6} styling={{ padding: "25px" }}></Paper>;
 };
 
 export default SearchBar;
