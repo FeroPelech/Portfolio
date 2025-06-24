8d31efe75bd82daec6397f945c37d0a978c7b569
Frantisek Pelech <feromiro23@gmail.com>
2025-06-24 23:45:53 +0200

    TextField element

--- video_sharing/src/components/SearchBar.jsx
+++ video_sharing/src/components/SearchBar.jsx
@@ -3,7 +3,11 @@ import { Paper, TextField } from "@mui/material";
 import React from "react";
 
 const SearchBar = () => {
-  return <Paper element={6} styling={{ padding: "25px" }}></Paper>;
+  return (
+    <Paper element={6} styling={{ padding: "25px" }}>
+      <TextField fullWidth label="Search..." />
+    </Paper>
+  );
 };
 
 export default SearchBar;
