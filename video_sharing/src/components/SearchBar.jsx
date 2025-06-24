4cda71a3bd4817b29a453130c1d27629a5f8634d
Frantisek Pelech <feromiro23@gmail.com>
2025-06-24 23:42:38 +0200

    SearchBar constant

--- video_sharing/src/components/SearchBar.jsx
+++ video_sharing/src/components/SearchBar.jsx
@@ -1,3 +1,9 @@
 import { useState } from "react";
 import { Paper, TextField } from "@mui/material";
 import React from "react";
+
+const SearchBar = () => {
+  return <div>SearchBar</div>;
+};
+
+export default SearchBar;
