ccb90541d8edd13f6bff0d9c7c08b9973c784a11
Frantisek Pelech <feromiro23@gmail.com>
2025-07-02 16:37:42 +0200

    Added on Add props to the CreateArea component

--- to_do_list/src/App.js
+++ to_do_list/src/App.js
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./components/Header";
 import CreateArea from "./components/CreateArea";
 
 const App = () => {
+  const [notes, setNotes] = useState([]);
+
   return (
     <div className="App">
       <Header />
-      <CreateArea />
+      <CreateArea onAdd={addNote} />
     </div>
   );
 };
