c5d4a8a010c92f7dfa2ea16c214c04dde33f9c30
Frantisek Pelech <feromiro23@gmail.com>
2025-07-28 03:54:21 +0200

    Import EmojiResults to the App.js

--- emoji_search/src/App.js
+++ emoji_search/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { PureComponent } from "react";
 import Header from "./Dashboard/Header/Header.jsx";
 import SearchBar from "./Dashboard/Searchbar/SearchBar.jsx";
-// import EmojiResults from "./Dashboard/Emojis/EmojiResults.jsx";
+import EmojiResults from "./Dashboard/Emojis/EmojiResults.jsx";
 import Filter from "./Dashboard/Emojis/Filter.js";
 
 class App extends PureComponent {
