2f1be414f543e179af61c597e5f3b27fabcf7eef
Frantisek Pelech <feromiro23@gmail.com>
2025-07-29 03:54:42 +0200

    Constant codePointHex

--- emoji_search/src/Dashboard/Emojis/EmojiResultRow.jsx
+++ emoji_search/src/Dashboard/Emojis/EmojiResultRow.jsx
@@ -3,5 +3,6 @@ import { PureComponent } from "react";
 export default class EmojiResultRow extends PureComponent {
   render() {
     const { title, symbol } = this.props;
+    const codePointHex = symbol.codePointAt(0).toString(16);
   }
 }
