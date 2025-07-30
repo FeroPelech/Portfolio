07db85535bd9a72eb9e7c6d67564abd1d4f58506
Frantisek Pelech <feromiro23@gmail.com>
2025-07-30 02:51:52 +0200

    Class name for main div

--- emoji_search/src/Dashboard/Emojis/EmojiResultRow.jsx
+++ emoji_search/src/Dashboard/Emojis/EmojiResultRow.jsx
@@ -6,7 +6,7 @@ export default class EmojiResultRow extends PureComponent {
     const codePointHex = symbol.codePointAt(0).toString(16);
     const src = `//cdn.jsdelivr.net/emojione/assets/png/${codePointHex}.png`;
     return (
-      <div>
+      <div className="component-emoji-result-row">
         <img src={src} alt={title} />
         <span className="title">{title}</span>
         <span className="info">Click to copy</span>
