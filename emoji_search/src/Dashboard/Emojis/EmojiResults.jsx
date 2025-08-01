be8160f580d89d7d0912b44425811edc3542f52c
Frantisek Pelech <feromiro23@gmail.com>
2025-08-01 18:30:42 +0200

    Small correction 1

--- emoji_search/src/Dashboard/Emojis/EmojiResults.jsx
+++ emoji_search/src/Dashboard/Emojis/EmojiResults.jsx
@@ -1,25 +1,27 @@
 import { PureComponent } from "react";
-import EmojiResultsRow from "./EmojiResultRow";
+import EmojiResultsRow from "./EmojiResultRow.jsx";
 import Clipboard from "clipboard";
 
 export default class EmojiResults extends PureComponent {
   componentDidMount() {
     this.clipboard = new Clipboard(".copy-it");
   }
+
   componentWillUnmount() {
     this.clipboard.destroy();
   }
+
   render() {
     const { emojiData } = this.props;
     return (
       <div className="component-emoji-results">
-        {emojiData.map((emoji) => {
+        {emojiData.map((emoji) => (
           <EmojiResultsRow
             key={emoji.title}
             symbol={emoji.symbol}
             title={emoji.title}
-          />;
-        })}
+          />
+        ))}
       </div>
     );
   }
