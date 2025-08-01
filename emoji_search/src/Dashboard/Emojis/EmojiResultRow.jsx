a5f7b1300d0dd615344be069197eca1c5db82633
Frantisek Pelech <feromiro23@gmail.com>
2025-08-01 09:36:08 +0200

    data-clipboard-text set symbol

--- emoji_search/src/Dashboard/Emojis/EmojiResultRow.jsx
+++ emoji_search/src/Dashboard/Emojis/EmojiResultRow.jsx
@@ -7,7 +7,10 @@ export default class EmojiResultRow extends PureComponent {
     const codePointHex = symbol.codePointAt(0).toString(16);
     const src = `//cdn.jsdelivr.net/emojione/assets/png/${codePointHex}.png`;
     return (
-      <div className="component-emoji-result-row">
+      <div
+        className="component-emoji-result-row copy-it "
+        data-clipboard-text={symbol}
+      >
         <img src={src} alt={title} />
         <span className="title">{title}</span>
         <span className="info">Click to copy</span>
