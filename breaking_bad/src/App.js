cb67013f174cb0c5f27f3959a934a3cf229d80f7
Frantisek Pelech <feromiro23@gmail.com>
2025-08-08 08:49:06 +0200

    Import CharacterList from CharacterList component

--- breaking_bad/src/App.js
+++ breaking_bad/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "./Components/NavBar";
 import SearchBar from "./Components/SearchBar";
+import CharacterList from "./Components/CharacterList";
 
 const App = () => {
   return <div className="App">App</div>;
