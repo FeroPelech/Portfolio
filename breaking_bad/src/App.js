0dcdad3a06377d80ba918b020be862e1729db2f1
Frantisek Pelech <feromiro23@gmail.com>
2025-08-08 08:48:18 +0200

    Import SearchBar from SearchBar component

--- breaking_bad/src/App.js
+++ breaking_bad/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from "./Components/NavBar";
+import SearchBar from "./Components/SearchBar";
 
 const App = () => {
   return <div className="App">App</div>;
