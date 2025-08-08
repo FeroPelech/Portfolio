c2300aaf5c0f0d371f3ced8f8a50ee7b3de3f993
Frantisek Pelech <feromiro23@gmail.com>
2025-08-08 20:06:49 +0200

    Constant for useState

--- breaking_bad/src/Components/SearchBar.jsx
+++ breaking_bad/src/Components/SearchBar.jsx
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 
-const SearchBar = () => {
+const SearchBar = ({ setQuery }) => {
+  const [text, setText] = useState("");
+
   return (
     <div>
       <section>
