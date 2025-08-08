04774bdfacee166a89e27a6078ccc8e90340f9e8
Frantisek Pelech <feromiro23@gmail.com>
2025-08-08 08:58:31 +0200

    UseState for items array set on empty array

--- breaking_bad/src/App.js
+++ breaking_bad/src/App.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Components/NavBar";
 import SearchBar from "./Components/SearchBar";
 import CharacterList from "./Components/CharacterList";
 
 const App = () => {
+  const [items, setItems] = useState([]);
   return (
     <div className="App">
       <Header />
