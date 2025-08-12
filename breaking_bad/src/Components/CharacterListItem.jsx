6480b3584f546cf1c511e87044a3117b1716fea2
Frantisek Pelech <feromiro23@gmail.com>
2025-08-12 20:53:56 +0200

    Added class to the first div

--- breaking_bad/src/Components/CharacterListItem.jsx
+++ breaking_bad/src/Components/CharacterListItem.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const CharacterListItem = () => {
-  return <div>CharacterListItem</div>;
+  return <div className="card">CharacterListItem</div>;
 };
 
 export default CharacterListItem;
