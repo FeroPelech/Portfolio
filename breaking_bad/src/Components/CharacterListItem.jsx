c1123f398883a2a402e95d824bdbba78c7950ac7
Frantisek Pelech <feromiro23@gmail.com>
2025-08-12 20:54:44 +0200

    Added new div with class card-front

--- breaking_bad/src/Components/CharacterListItem.jsx
+++ breaking_bad/src/Components/CharacterListItem.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const CharacterListItem = () => {
-  return <div className="card">CharacterListItem</div>;
+  return (
+    <div className="card">
+      <div className="card-front"></div>
+    </div>
+  );
 };
 
 export default CharacterListItem;
