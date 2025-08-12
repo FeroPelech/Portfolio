b074ed7ac2524c01f21265315309e9a6425928d0
Frantisek Pelech <feromiro23@gmail.com>
2025-08-12 19:48:55 +0200

    Added imtems prop to the Character list component in App.js

--- breaking_bad/src/App.js
+++ breaking_bad/src/App.js
@@ -22,7 +22,7 @@ const App = () => {
     <div className="App">
       <Header />
       <SearchBar setQuery={(query) => setQuery(query)} />
-      <CharacterList />
+      <CharacterList items={items} />
     </div>
   );
 };
