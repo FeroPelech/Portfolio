1cafa522c3184cca78dc2f4d62c642c01b4daee0
Frantisek Pelech <feromiro23@gmail.com>
2025-08-12 20:55:43 +0200

    Added image

--- breaking_bad/src/Components/CharacterListItem.jsx
+++ breaking_bad/src/Components/CharacterListItem.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 
-const CharacterListItem = () => {
+const CharacterListItem = ({ item }) => {
   return (
     <div className="card">
-      <div className="card-front"></div>
+      <div className="card-front">
+        <img src={item.img} alt="character" />
+      </div>
     </div>
   );
 };
