b7dce2fce9235d6c4e032ea9e9e370c55bf2afa6
Frantisek Pelech <feromiro23@gmail.com>
2025-08-20 20:14:56 +0200

    Deleted old project

--- breaking_bad/src/Components/CharacterList.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import CharacterListItem from "./CharacterListItem";
-
-const CharacterList = ({ items }) => {
-  return items.map((item) => <CharacterListItem />);
-};
-
-export default CharacterList;
