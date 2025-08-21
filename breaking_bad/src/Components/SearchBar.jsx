e2a2da27dca9ce9a291ef62ace7ebb3f4a9baa4a
Frantisek Pelech <feromiro23@gmail.com>
2025-08-21 21:27:33 +0200

    Set query value

--- breaking_bad/src/Components/SearchBar.jsx
+++ breaking_bad/src/Components/SearchBar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const SearchBar = ({ query, setQuery }) => {
   return (
     <div>
-      <input type="text" />
+      <input type="text" value={query} />
     </div>
   );
 };
