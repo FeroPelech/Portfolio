07dc83d667407a6be3b809c023342bb520c41c3a
Frantisek Pelech <feromiro23@gmail.com>
2025-08-21 10:16:29 +0200

    Added query like a call element

--- breaking_bad/src/Hooks/useCharacters.js
+++ breaking_bad/src/Hooks/useCharacters.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
-const useCharacters = () => {
+const useCharacters = (query) => {
   const [characters, setCharacters] = useState([]);
   const [loading, setLoading] = useState(false);
 
