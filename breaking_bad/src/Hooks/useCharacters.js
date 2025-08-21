0f566ac0db6013c14c67fda194cdc43007b0698a
Frantisek Pelech <feromiro23@gmail.com>
2025-08-21 10:07:25 +0200

    Use state for characters set on empty array

--- breaking_bad/src/Hooks/useCharacters.js
+++ breaking_bad/src/Hooks/useCharacters.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 
 const useCharacters = () => {
+  const [characters, setCharacters] = useState([]);
   return <div>useCharacters</div>;
 };
 
