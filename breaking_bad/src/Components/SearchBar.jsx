4b303f6c306d718b605a3d38a4f8b5d34dd6cdcc
Frantisek Pelech <feromiro23@gmail.com>
2025-08-21 21:28:25 +0200

    Set onChange

--- breaking_bad/src/Components/SearchBar.jsx
+++ breaking_bad/src/Components/SearchBar.jsx
@@ -3,7 +3,11 @@ import React from "react";
 const SearchBar = ({ query, setQuery }) => {
   return (
     <div>
-      <input type="text" value={query} />
+      <input
+        type="text"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
     </div>
   );
 };
