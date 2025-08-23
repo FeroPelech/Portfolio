4ccc333f9db16712e3f53eea0ac6b3af6b418154
Frantisek Pelech <feromiro23@gmail.com>
2025-08-23 09:55:53 +0200

    Added class name

--- breaking_bad/src/Components/CharacterList.jsx
+++ breaking_bad/src/Components/CharacterList.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import CharacterCard from "./CharacterCard";
 
 const CharacterList = () => {
-  return <div>CharacterList</div>;
+  return <div className="character-list">CharacterList</div>;
 };
 
 export default CharacterList;
