b4de7fd6dea8681a33c88059933edfdeb15a617d
Frantisek Pelech <feromiro23@gmail.com>
2025-08-24 21:07:13 +0200

    Imported useCharacters

--- breaking_bad/src/App.js
+++ breaking_bad/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import SearchBar from "./Components/SearchBar";
 import CharacterList from "./Components/CharacterList";
+import useCharacters from "./Hooks/useCharacters";
 
 const App = () => {
   return <div>App</div>;
