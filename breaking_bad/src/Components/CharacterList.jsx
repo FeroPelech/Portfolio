723f53d573fbd5c519d833ff37d1071bd382a319
Frantisek Pelech <feromiro23@gmail.com>
2025-08-24 20:52:47 +0200

    Added CharacterCard

--- breaking_bad/src/Components/CharacterList.jsx
+++ breaking_bad/src/Components/CharacterList.jsx
@@ -2,7 +2,13 @@ import React from "react";
 import CharacterCard from "./CharacterCard";
 
 const CharacterList = ({ characters }) => {
-  return <div className="character-list">{characters.map((char)=>())}</div>;
+  return (
+    <div className="character-list">
+      {characters.map((char) => (
+        <CharacterCard key={char.char_id} character={char} />
+      ))}
+    </div>
+  );
 };
 
 export default CharacterList;
