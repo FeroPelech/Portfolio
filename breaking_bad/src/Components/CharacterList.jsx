e7b0eceb70deb99aaf229140808de067f3e3f624
Frantisek Pelech <feromiro23@gmail.com>
2025-08-24 20:48:35 +0200

    Map loop

--- breaking_bad/src/Components/CharacterList.jsx
+++ breaking_bad/src/Components/CharacterList.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import CharacterCard from "./CharacterCard";
 
 const CharacterList = ({ characters }) => {
-  return <div className="character-list"></div>;
+  return <div className="character-list">{characters.map((char)=>())}</div>;
 };
 
 export default CharacterList;
