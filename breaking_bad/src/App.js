93e6fc5f4a525ab250733d1e2c63eb0e2d541580
Frantisek Pelech <feromiro23@gmail.com>
2025-08-25 04:47:00 +0200

    Correction

--- breaking_bad/src/App.js
+++ breaking_bad/src/App.js
@@ -1,16 +1,10 @@
-import React, { useState } from "react";
 import SearchBar from "./Components/SearchBar";
-import CharacterList from "./Components/CharacterList";
-import useCharacters from "./Hooks/useCharacters";
 
 const App = () => {
-  const [query, setQuery] = useState("");
-  const { characters, loading } = useCharacters(query);
   return (
     <div className="App">
       <h1>Breaking Bad Characters</h1>
-      <SearchBar query={query} setQuery={setQuery} />
-      {loading ? <p>Loading...</p> : <CharacterList characters={characters} />}
+      <SearchBar />
     </div>
   );
 };
