e654a05fe1faf7b5500fa521c131dbae11d9dd16
Frantisek Pelech <feromiro23@gmail.com>
2025-09-01 06:14:07 +0200

    Import Quotes to the App.jsx

--- random_quote/src/App.jsx
+++ random_quote/src/App.jsx
@@ -1,7 +1,8 @@
 import React from "react";
+import Quotes from "./Dashboard/Quotes/Quotes";
 
 const App = () => {
-  return <div className="App">App</div>;
+  return <div className="App"></div>;
 };
 
 export default App;
