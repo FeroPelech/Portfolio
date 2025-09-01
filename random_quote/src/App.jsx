d61bd79728ebf9e5d649d7d5b1b84d701867dc06
Frantisek Pelech <feromiro23@gmail.com>
2025-09-01 06:38:20 +0200

    Imported QuotesAuthor

--- random_quote/src/App.jsx
+++ random_quote/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Quotes from "./Dashboard/Quotes/Quotes";
 import Buttons from "./Dashboard/Components/Buttons";
 import Footer from "./Dashboard/Components/Footer";
+import QuoteAuthor from "./Dashboard/Components/QuoteAuthor";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Quotes />
       <Buttons />
       <Footer />
+      <QuoteAuthor />
     </div>
   );
 };
