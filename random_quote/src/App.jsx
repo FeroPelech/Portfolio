b690a93f5344a06c081700becada6215ebb45291
Frantisek Pelech <feromiro23@gmail.com>
2025-09-01 06:36:39 +0200

    Imported Footer

--- random_quote/src/App.jsx
+++ random_quote/src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Quotes from "./Dashboard/Quotes/Quotes";
 import Buttons from "./Dashboard/Components/Buttons";
+import Footer from "./Dashboard/Components/Footer";
 
 const App = () => {
   return (
     <div className="App">
       <Quotes />
       <Buttons />
+      <Footer />
     </div>
   );
 };
