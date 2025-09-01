5c75e17f4ff9afbc812eb440662141cca37aa4db
Frantisek Pelech <feromiro23@gmail.com>
2025-09-01 06:31:02 +0200

    Imported Buttons

--- random_quote/src/App.jsx
+++ random_quote/src/App.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import Quotes from "./Dashboard/Quotes/Quotes";
+import Buttons from "./Dashboard/Components/Buttons";
 
 const App = () => {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Quotes />
+      <Buttons />
+    </div>
+  );
 };
 
 export default App;
