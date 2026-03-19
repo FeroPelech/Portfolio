6a6b01c7541aa15ed9877e1bdc5b576297fa0971
FeroPelech <feromiro23@gmail.com>
2026-03-19 13:40:02 +0100

    Just importing first player

--- pexeso/src/App.jsx
+++ pexeso/src/App.jsx
@@ -1,5 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
+import players from './ListOfPlayers'
 
 export default function App() {
-  return <div>App</div>
+  players.map((e) => {
+    console.log(e)
+  })
+  return (
+    <div>
+      <img src={players[0].link} alt={players[0].name} />
+    </div>
+  )
 }
---
+++ pexeso/src/ListOfPlayers.js
@@ -0,0 +1,11 @@
+import tour1 from './players/WilliamSaliba.png'
+
+const players = [
+  {
+    id: 1,
+    name: 'Saliba',
+    link: tour1,
+  },
+]
+
+export default players
