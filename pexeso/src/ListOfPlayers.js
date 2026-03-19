a3898d073422a3c69d4c1caec48ae31ce4057def
FeroPelech <feromiro23@gmail.com>
2026-03-19 17:02:27 +0100

    Importet all players

--- pexeso/src/ListOfPlayers.js
+++ pexeso/src/ListOfPlayers.js
@@ -1,10 +1,32 @@
-import tour1 from './players/WilliamSaliba.png'
+import white from './players/BenWhite.png'
+import saka from './players/BukayoSaka.png'
+import norgaard from './players/ChristianNorgaard.png'
+import mosquera from './players/CristhianMosquera.png'
+import raya from './players/DavidRaya.png'
+import rice from './players/DeclanRice.png'
+import eze from './players/EberechiEze.png'
+import gabriel from './players/GabrielMagalhaes.png'
+import martinelli from './players/GabrielMartinelli.png'
+import jurrien from './players/JurrienTimber.png'
+import kai from './players/KaiHavertz.png'
+import leandro from './players/LeandroTrosard.png'
+import miles from './players/Lewis-Skelly.png'
+import martinO from './players/MartinOdegaard.png'
+import martinZ from './players/MartinZubimendi.png'
+import max from './players/MaxDowman.png'
+import couch from './players/MikelArteta.png'
+import mikel from './players/MikelMerino.png'
+import noni from './players/NoniMadueke.png'
+import piero from './players/PieroHincapie.png'
+import riccardo from './players/RiccardoCalafiori.png'
+import viktor from './players/ViktorGyokeres.png'
+import william from './players/WilliamSaliba.png'
 
 const players = [
   {
     id: 1,
-    name: 'Saliba',
-    link: tour1,
+    name: 'Ben White',
+    link: white,
   },
 ]
 
