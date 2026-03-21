5d0bad5fd4f69c60b5c21b0542ca0b9292819a3d
FeroPelech <feromiro23@gmail.com>
2026-03-21 12:20:06 +0100

    Check images

--- pexeso/src/App.jsx
+++ pexeso/src/App.jsx
@@ -1,13 +1,13 @@
-import React, { useState } from 'react'
 import players from './ListOfPlayers'
 
 export default function App() {
-  players.map((e) => {
-    console.log(e)
-  })
   return (
     <div>
-      <img src={players[0].link} alt={players[0].name} />
+      {players.map((e) => (
+        <div key={e.id}>
+          <img src={e.link} alt={e.name} />
+        </div>
+      ))}
     </div>
   )
 }
